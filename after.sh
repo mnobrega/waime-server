407200e589912ed6c807de03bbf6aab00b039576
Márcio Nóbrega <marcio.nobrega@premium-minds.com>
2017-07-07 17:09:17 +0100

    Added Homestead.yaml example and postgres external configuration.

--- after.sh
+++ after.sh
@@ -4,5 +4,10 @@
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
 
+#bash path
 echo 'export PATH="./vendor/bin:$PATH"' >>  ~/.bashrc
 cd ~/Code/waime-server
+
+#postgres
+sudo cp .provision/postgresql/pg_hba.conf /etc/postgresql/9.5/main/pg_hba.conf
+sudo service postgresql restart
